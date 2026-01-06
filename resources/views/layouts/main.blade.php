6e30ffe6370dd7d16b3fe4683b475f4fbe53eafb
teknologiimss <teknologi.imss@gmail.com>
2026-01-06 10:51:55 +0700

    balik awal

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1880,22 +1880,5 @@
     {{-- @section('scripts')
     @endsection --}}
 </body>
-<div class="container mt-3">
-
-    @if (session('success'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ session('success') }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-        </div>
-    @endif
-
-    @if (session('error'))
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            {{ session('error') }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-        </div>
-    @endif
-
-</div>
 
 </html>
