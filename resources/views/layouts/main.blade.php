7ec1ca80a88860ab208298ca7ef13fa20850cf77
teknologiimss <teknologi.imss@gmail.com>
2026-01-06 11:00:36 +0700

    coba notif scan

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -749,6 +749,24 @@
             </div>
         </aside>
 
+        <div class="container mt-3">
+
+    @if (session('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+        </div>
+    @endif
+
+    @if (session('error'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            {{ session('error') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+        </div>
+    @endif
+
+</div>
+
         <div class="content-wrapper">
             @yield('content')
         </div>
