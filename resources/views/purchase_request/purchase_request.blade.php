6bbb83e9716b208e010ad255c44d1367f5943d5b
renatatristiana <tristirenata@gmail.com>
2026-03-05 15:25:53 +0700

    perbaikan double filed catatan

--- resources/views/purchase_request/purchase_request.blade.php
+++ resources/views/purchase_request/purchase_request.blade.php
@@ -556,12 +556,12 @@
                                     </div>
                                 </div>
 
-                                <div class="form-group row">
+                                <!-- <div class="form-group row">
                                     <label for="catatan" class="col-sm-4 col-form-label">Catatan</label>
                                     <div class="col-sm-8">
                                         <textarea class="form-control" id="catatan" name="catatan" rows="3"></textarea>
                                     </div>
-                                </div>
+                                </div> -->
 
                                 <div class="form-group row">
                                     <label for="revisi" class="col-sm-4 col-form-label">Revisi</label>
